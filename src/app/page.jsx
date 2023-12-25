ca18c9ad7ec134003469c63ef03bc313ffb7a29b
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-12-25 09:06:06 +0100

    initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import ScrollSection from '@/components/ScrollSection'
+import Hero from "@/components/Hero";
+import React from 'react'
+
+
+export default function Home() {
+  return (
+    <>
+    <Hero />
+    <ScrollSection />
+   </>
+    )
+}
---
+++ src/components/ScrollSection.jsx
@@ -0,0 +1,47 @@
+import React, {useRef, useEffect} from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+export default function ScrollSection() {
+
+    const sectionRef = useRef(null);
+    const triggerRef = useRef(null);
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+      
+        const pin = gsap.fromTo(sectionRef.current, {
+            translateX: 0
+        }, {
+            translateX: "-300vh",
+            ease: "none",
+            duration: 1,
+            scrollTrigger: {
+                trigger: triggerRef.current,
+                start: "top top",
+                end: "2000 top",
+                scrub: true,
+                pin: true
+            }
+        })
+    
+      return () => {
+        pin.kill()
+      }
+    }, [])
+    
+
+  return (
+   <section className="scroll-section-outer">
+    <div ref={triggerRef}>
+        <div ref={sectionRef} className="scroll-section-inner">
+            <div className="scroll-section"><h3>section 1</h3></div>
+            <div className="scroll-section"><h3>section 2</h3></div>
+            <div className="scroll-section"><h3>section 3</h3></div>
+            <div className="scroll-section"><h3>section 4</h3></div>
+        </div>
+    </div>
+   </section>
+  )
+}
